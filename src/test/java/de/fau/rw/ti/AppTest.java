2fdec01fb1114df09b8013d54cf4b4641075be83
Daniel Schraudner <daniel.schraudner@fau.de>
2021-05-28 11:18:05 +0200

    Init

---
+++ src/test/java/de/fau/rw/ti/AppTest.java
@@ -0,0 +1,20 @@
+package de.fau.rw.ti;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
